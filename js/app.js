a626ecd3b23edba93ab459b5956d91419e39a0ee
Deepak Guptha <vzan2012@gmail.com>
2020-09-05 22:45:03 +0530

    chore: Updation of project config and refactoring

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 var num1 = document.querySelector('#num1');
 var num2 = document.querySelector('#num2');
 var button = document.querySelector('button');
-var add = function (num1, num2) { return num1 + num2; };
+var add = function (input1, input2) { return input1 + input2; };
 button.addEventListener('click', function () { return console.log(add(+num1.value, +num2.value)); });
--- js/app.ts
+++ js/app.ts
@@ -2,6 +2,6 @@ const num1 = document.querySelector('#num1')! as HTMLInputElement
 const num2 = document.querySelector('#num2')! as HTMLInputElement
 const button = document.querySelector('button')
 
-const add = (num1: number, num2: number): number => num1 + num2
+const add = (input1: number, input2: number): number => input1 + input2
 
 button.addEventListener('click', () => console.log(add(+ num1.value, + num2.value)));
