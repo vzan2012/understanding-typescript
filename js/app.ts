bc45aa402782ace1e5f762203b4e7c72289a10fd
Deepak Guptha <vzan2012@gmail.com>
2020-09-06 18:55:44 +0530

    feat: Updation of typescript basics

--- js/app.ts
+++
@@ -1,7 +0,0 @@
-const num1 = document.querySelector('#num1')! as HTMLInputElement
-const num2 = document.querySelector('#num2')! as HTMLInputElement
-const button = document.querySelector('button')
-
-const add = (input1: number, input2: number): number => input1 + input2
-
-button.addEventListener('click', () => console.log(add(+ num1.value, + num2.value)));
--- js/basics.ts
+++ js/basics.ts
@@ -1,20 +1,24 @@
 // const num1 = document.querySelector('#num1')! as HTMLInputElement
 // const num2 = document.querySelector('#num2')! as HTMLInputElement
 // const button = document.querySelector('button')
+
 // const add = (input1: number, input2: number): number => input1 + input2
+
 // button.addEventListener('click', () => console.log(add(+ num1.value, + num2.value)));
+
 // Example (C 14)
-var add = function (n1, n2, showResult, phrase) {
-    var result = n1 + n2;
+const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
+    const result = n1 + n2;
     if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
+        console.log(phrase + result)
+    } else {
+        return result
     }
 };
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = 'Result is ';
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is ';
+
 add(number1, number2, printResult, resultPhrase);
