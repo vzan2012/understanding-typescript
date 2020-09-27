372c8cd43a8e543aad56315ed3fa72b624315c16
Deepak Guptha <vzan2012@gmail.com>
2020-09-27 15:21:44 +0530

    feat: TypeScript Classes

--- dist/app.js
+++ dist/app.js
@@ -1,64 +1,35 @@
 "use strict";
-// Next-gen Scripts
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-// let and const
-var userNameUsed = 'vzan2012';
-var userAge = 102;
-// userName = 'other';
-console.log(userNameUsed);
-console.log(userAge);
-// Arrow function 
-var addition = function (a, b) {
-    if (b === void 0) { b = 5; }
-    return a + b;
-};
-// A default parameter should be used at the last 
-// console.log(add(5, 5))
-var printOut = function (output) { return console.log(output); };
-printOut(addition(5, 7));
-printOut(addition(5));
-var buttonClick = document.querySelector('button');
-buttonClick === null || buttonClick === void 0 ? void 0 : buttonClick.addEventListener('click', function (event) { return console.log(event); });
-// Spread Operator 
-var hobbies = ['Reading', 'Playing'];
-var activeHobbies = ['Sports'];
-activeHobbies.push.apply(activeHobbies, hobbies);
-console.log(activeHobbies);
-var person = { userName: 'vzan2012', age: 102 };
-var replicateUser = __assign({}, person);
-console.log("Person Object: ");
-console.log(person);
-console.log("Replicated User Object: ");
-console.log(replicateUser);
-// REST parameters 
-// const add_N_numbers = (...nums: number[]): number => {
-//     return nums.reduce((currValue, resultValue) => {
-//         return currValue + resultValue
-//     }, 0)
-// };
-var add_N_numbers = function () {
-    var nums = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        nums[_i] = arguments[_i];
+// Classes
+var Department = /** @class */ (function () {
+    // Shorthand Initialization 
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        // Access Modifiers
+        // private readonly id: string;
+        // public name: string;
+        this.employees = [];
+        // this.id = id;
+        // this.name = name;
     }
-    return nums.reduce(function (currValue, resultValue) { return currValue + resultValue; }, 0);
-};
-console.log(add_N_numbers(4, 5, 6, 7, 8, 9));
-// Array and Object Destructuring
-// const hobby1 = hobbies[0]
-// const hobby2 = hobbies[1]
-var hobby1 = activeHobbies[0], hobby2 = activeHobbies[1], remainingHobbies = activeHobbies.slice(2);
-console.log(hobby1, hobby2, remainingHobbies);
-var userName = replicateUser.userName, age = replicateUser.age;
-console.log(userName, age);
+    Department.prototype.describe = function () {
+        console.log("Department: (" + this.id + "): " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
+var department = new Department('D1', 'Accounting');
+console.log(department);
+department.describe();
+department.addEmployee('Sai Shravan');
+department.addEmployee('vzan2012');
+department.printEmployeeInformation();
+// const accountingCopy = { name: 'Testing', describe: department.describe }
+// accountingCopy.describe()
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,54 +1,38 @@
-// Next-gen Scripts
+// Classes
+class Department {
+    // Access Modifiers
+    // private readonly id: string;
+    // public name: string;
+    private employees: string[] = [];
 
+    // Shorthand Initialization 
+    constructor(private readonly id: string, public name: string) {
+        // this.id = id;
+        // this.name = name;
+    }
 
-// let and const
-const userNameUsed = 'vzan2012';
-let userAge = 102;
-// userName = 'other';
-console.log(userNameUsed);
-console.log(userAge)
+    describe(this: Department) {
+        console.log(`Department: (${this.id}): ${this.name}`)
+    }
 
+    addEmployee(employee: string) {
+        this.employees.push(employee)
+    }
 
-// Arrow function 
-const addition = (a: number, b: number = 5) => a + b;
-// A default parameter should be used at the last 
-// console.log(add(5, 5))
-const printOut = (output: string | number) => console.log(output);
-printOut(addition(5, 7))
-printOut(addition(5))
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees)
+    }
+}
 
-const buttonClick = document.querySelector('button');
-buttonClick?.addEventListener('click', event => console.log(event))
+const department = new Department('D1', 'Accounting');
+console.log(department);
+department.describe();
 
+department.addEmployee('Sai Shravan');
+department.addEmployee('vzan2012');
 
-// Spread Operator 
-const hobbies = ['Reading', 'Playing'];
-const activeHobbies = ['Sports'];
-activeHobbies.push(...hobbies)
-console.log(activeHobbies);
+department.printEmployeeInformation()
 
-const person = { userName: 'vzan2012', age: 102 }
-const replicateUser = { ...person };
-console.log(`Person Object: `)
-console.log(person);
-console.log(`Replicated User Object: `)
-console.log(replicateUser);
-
-
-// REST parameters 
-// const add_N_numbers = (...nums: number[]): number => {
-//     return nums.reduce((currValue, resultValue) => {
-//         return currValue + resultValue
-//     }, 0)
-// };
-const add_N_numbers = (...nums: number[]) => nums.reduce((currValue, resultValue) => currValue + resultValue, 0)
-console.log(add_N_numbers(4, 5, 6, 7, 8, 9))
-
-
-// Array and Object Destructuring
-// const hobby1 = hobbies[0]
-// const hobby2 = hobbies[1]
-const [hobby1, hobby2, ...remainingHobbies] = activeHobbies
-console.log(hobby1, hobby2, remainingHobbies);
-const { userName, age } = replicateUser
-console.log(userName, age)
+// const accountingCopy = { name: 'Testing', describe: department.describe }
+// accountingCopy.describe()
