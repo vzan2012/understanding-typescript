03feeeb15760d13086e66c0716d918c9a43f21c2
Deepak Guptha <vzan2012@gmail.com>
2020-10-18 15:44:10 +0530

    feat: Decorator declaration & its factories

--- dist/app.js
+++ dist/app.js
@@ -1,90 +1,32 @@
 "use strict";
-// Generics
-// const names: Array<string> = ['']  // string[]
-// const promise: Promise<string> = new Promise((resolve, reject) => {
-//     try {
-//         setTimeout(() => {
-//             resolve('Done !!!')
-//         }, 2000)
-//     }
-//     catch (e) {
-//         reject(e)
-//     }
-// })
-// promise.then((data) => {
-//     console.log(data)
-// })
-// Creating Generic functions 
-// Merging of two objects
-// Adding the constraints
-function merge(objA, objB) {
-    return Object.assign(objA, objB);
-}
-// const mergedObj = merge({ name: 'vzan2012', hobbies: ['Coding'] }, { age: 100 })
-const mergedObj = merge({ name: 'vzan2012', hobbies: ['Coding'] }, { age: 30 });
-console.log(mergedObj);
-console.log(mergedObj.name);
-console.log(mergedObj.hobbies);
-console.log(mergedObj.age);
-// Another Generic function 
-function countAndDescribe(element) {
-    let describeTxt = 'No Value !!!';
-    if (element.length === 1) {
-        describeTxt = 'Has 1 element';
-    }
-    else if (element.length > 1) {
-        describeTxt = `Has ${element.length} elements`;
-    }
-    return [element, describeTxt];
-}
-console.log(countAndDescribe('Hello Sai'));
-console.log(countAndDescribe(['Reading', 'Coding']));
-console.log(countAndDescribe([]));
-// keyOf - Constraint 
-function extractAndConvert(obj, key) {
-    return `Value of ${obj[key]}`;
-}
-console.log(extractAndConvert({ name: 'Sai' }, 'name'));
-// Only the primitive types 
-class DataStorage {
-    constructor() {
-        this.data = [];
-    }
-    addItem(item) {
-        this.data.push(item);
-    }
-    removeItem(item) {
-        if (this.data.indexOf(item) === -1)
-            return;
-        this.data.splice(this.data.indexOf(item), 1);
-    }
-    getItem() {
-        return [...this.data];
-    }
-}
-const txtStorage = new DataStorage();
-txtStorage.addItem('vzan2012');
-txtStorage.addItem('Sai');
-txtStorage.removeItem('vzan2012');
-console.log(txtStorage.getItem());
-const numberStorage = new DataStorage();
-numberStorage.addItem(10);
-numberStorage.addItem(20);
-numberStorage.addItem(30);
-numberStorage.addItem(50);
-console.log(numberStorage.getItem());
-numberStorage.removeItem(50);
-console.log(numberStorage.getItem());
-const createCourseGoal = (title, description, date) => {
-    // Partial type 
-    let courseGoal = {};
-    courseGoal.title = title;
-    courseGoal.description = description;
-    courseGoal.completeUntil = date;
-    return courseGoal;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-// Readonly type 
-const nameStore = ['vzan2012', 'Sai'];
-// nameStore.push('Star')
-console.log(nameStore);
+// Decorators
+// function Logger(constructor: Function) {
+//     console.log('Logging ...')
+//     console.log(constructor)
+// }
+function Logger(logString) {
+    return function (constructor) {
+        console.log('Logging ...');
+        console.log(logString);
+        console.log(constructor);
+    };
+}
+// Adding a decorator 
+let Person = class Person {
+    constructor() {
+        this.name = "vzan2012";
+        console.log('Person object is created');
+    }
+};
+Person = __decorate([
+    Logger('Logging - Person')
+], Person);
+const personObj = new Person();
+console.log(personObj);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
 // Decorators
+// function Logger(constructor: Function) {
+//     console.log('Logging ...')
+//     console.log(constructor)
+// }
+function Logger(logString: string) {
+    return function (constructor: Function) {
+        console.log('Logging ...')
+        console.log(logString)
+        console.log(constructor)
+    }
+}
+
+// Adding a decorator 
+@Logger('Logging - Person')
+class Person {
+    name = "vzan2012";
+
+    constructor() {
+        console.log('Person object is created')
+    }
+}
+
+const personObj = new Person();
+console.log(personObj)
