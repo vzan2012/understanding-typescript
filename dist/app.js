32c2f7a28418a211ea62faa3d851e1912e509017
Deepak Guptha <vzan2012@gmail.com>
2020-10-18 20:23:37 +0530

    feat: Working on Decorators - Part 2

--- dist/app.js
+++ dist/app.js
@@ -11,10 +11,24 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 //     console.log(constructor)
 // }
 function Logger(logString) {
+    console.log('LOGGER FACTORY');
     return function (constructor) {
         console.log('Logging ...');
         console.log(logString);
         console.log(constructor);
+    };
+}
+function withTemplate(template, hookId) {
+    console.log('TEMPLATE FACTORY');
+    // return function (_: Function) {
+    return function (constructor) {
+        console.log('Rendering Template');
+        const hookElem = document.getElementById(hookId);
+        const username = new constructor();
+        if (hookElem) {
+            hookElem.innerHTML = template;
+            hookElem.querySelector('h1').textContent = username.name;
+        }
     };
 }
 // Adding a decorator 
@@ -25,8 +39,33 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger('Logging - Person')
+    Logger('Logging - Person'),
+    withTemplate('<h1>Person Object</h1>', 'app')
 ], Person);
 const personObj = new Person();
 console.log(personObj);
+function Log(target, propertyName) {
+    console.log('Property Decorator !!!');
+    console.log(target, propertyName);
+}
+class Product {
+    constructor(t, p) {
+        this.title = t;
+        this._price = p;
+    }
+    set price(val) {
+        if (val > 0) {
+            this._price = val;
+        }
+        else {
+            throw new Error('Price is Invalid - should be greater than zero');
+        }
+    }
+    getPriceWithTax(tax) {
+        return this._price * (1 + tax);
+    }
+}
+__decorate([
+    Log
+], Product.prototype, "title", void 0);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@
 //     console.log(constructor)
 // }
 function Logger(logString: string) {
+    console.log('LOGGER FACTORY')
     return function (constructor: Function) {
         console.log('Logging ...')
         console.log(logString)
@@ -11,8 +12,23 @@ function Logger(logString: string) {
     }
 }
 
+function withTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY')
+    // return function (_: Function) {
+    return function (constructor: any) {
+        console.log('Rendering Template')
+        const hookElem = document.getElementById(hookId);
+        const username = new constructor()
+        if (hookElem) {
+            hookElem.innerHTML = template
+            hookElem.querySelector('h1')!.textContent = username.name
+        }
+    }
+}
+
 // Adding a decorator 
 @Logger('Logging - Person')
+@withTemplate('<h1>Person Object</h1>', 'app')
 class Person {
     name = "vzan2012";
 
@@ -23,3 +39,31 @@ class Person {
 
 const personObj = new Person();
 console.log(personObj)
+
+function Log(target: any, propertyName: string | Symbol) {
+    console.log('Property Decorator !!!')
+    console.log(target, propertyName)
+}
+
+class Product {
+    @Log
+    title: string
+    private _price: number
+
+    set price(val: number) {
+        if (val > 0) {
+            this._price = val
+        } else {
+            throw new Error('Price is Invalid - should be greater than zero')
+        }
+    }
+
+    constructor(t: string, p: number) {
+        this.title = t;
+        this._price = p;
+    }
+
+    getPriceWithTax(tax: number) {
+        return this._price * (1 + tax)
+    }
+}
